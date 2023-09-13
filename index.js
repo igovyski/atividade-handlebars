469133a87f421140fa7a33bb22ca62406b522af1
Igor Pessoa <igor.silva26@outlook.com>
2023-09-13 17:05:45 -0300

    completo

--- index.js
+++ index.js
@@ -2,40 +2,42 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 
 const app = express()
+app.use(express.static('public'))
 
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
-const products = [
+const produtos = [
     {
-        link: "/camisa_corinthians",
+        link: "camisa_corinthians",
         nome: "Camisa Corinthians",
-        valor: "R$250,00"
+        valor: "R$299,00",
+        imagemUrl: "https://imgnike-a.akamaihd.net/1920x1920/02611151.jpg"
     },
     {   
-        link: "/chuteira_mercurial",
+        link: "chuteira_mercurial",
         nome: "Chuteira Nike Mercurial",
-        valor: "R$450,00"
+        valor: "R$450,00",
+        imagemUrl: "https://imgnike-a.akamaihd.net/1920x1920/022595IM.jpg"
     },
     {   
-        link: "/corta-vento_brasil",
+        link: "corta-vento_brasil",
         nome: "Corta-Vento Seleção Brasileira",
-        valor: "R$350,00"
+        valor: "R$350,00",
+        imagemUrl: "https://imgnike-a.akamaihd.net/1920x1920/02337615.jpg"
     }
 ]
 
 app.get('/', (req, res) => {
-    res.render('home', {products})
+    res.render('home', {produtos})
 })
 
 app.get('/:id', (req, res) => {
     const id = req.params.id
 
-    const product = products.map(product => {
-        return product.link === id
-    })
+    const produto = produtos.find((p) => p.link === id);
 
-    res.render('home')
+    res.render('detalhesProduto', {produto})
 })
 
 
